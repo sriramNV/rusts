e91eaa637aed440bd111e02ac417440d09ae86ca
sriram N V <sriramnvilvanathan@gmail.com>
2025-03-11 18:00:47 +0530

    arrays

--- learning/src/compound_data_type.rs
+++ learning/src/compound_data_type.rs
@@ -1,4 +1,12 @@
 fn compound_data_type(){
     
     // the compound data types in rust are arrays, tuples, slices and strings (slice strings)
+
+    // Arrays are collection of items of homogenous data type
+
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    let alphabets: [char; 10] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+    println!("numbers: {:?}", numbers);
+    println!("alphabets: {:?}", alphabets);
 }
--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,5 +1,7 @@
-include!("./fn scalar_data_type.rs");
+include!("./scalar_data_type.rs");
+include!("./compound_data_type.rs");
 
 fn main() {
     scalar_data_type();
+    compound_data_type();
 }
