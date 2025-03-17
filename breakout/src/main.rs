3a2ab9d8e331f86f029f39cc4c89a083d33da27c
sriram N V <sriramnvilvanathan@gmail.com>
2025-03-17 21:50:31 +0530

    added blocks and ball

--- breakout/src/main.rs
+++ breakout/src/main.rs
@@ -2,6 +2,9 @@ use macroquad::prelude::*;
 
 const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
 const PLAYER_SPEED: f32 = 700f32;
+const BLOCK_SIZE: Vec2 = Vec2::from_array([110f32, 40f32]);
+const BALL_SIZE: f32 = 50f32;
+const BALL_SPEED: f32 = 400f32;
 
 struct Player{
     rect: Rect,
@@ -49,14 +52,93 @@ impl Player{
     }
 }
 
+struct Block{
+    rect: Rect,
+}
+
+impl Block{
+    pub fn new(pos: Vec2) -> Self{
+        Self{
+            rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y),
+        }
+    }
+
+    pub fn draw(&self){
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, GRAY);
+    }
+}
+
+struct Ball{
+    rect: Rect,
+    vel: Vec2,
+}
+
+impl Ball{
+    pub fn new(pos: Vec2) -> Self{
+        Self{
+            rect: Rect::new(pos.x, pos.y, BALL_SIZE, BALL_SIZE),
+            vel: vec2(rand::gen_range(-1f32, 1f32), 1f32).normalize(),
+        }        
+    }
+    pub fn draw(&self){
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKGRAY);
+    }
+
+    pub fn update(&mut self, dt: f32){
+        self.rect.x += self.vel.x * dt * BALL_SPEED;
+        self.rect.y += self.vel.y * dt * BALL_SPEED;
+
+        if self.rect.x < 0f32{
+            self.vel.x = 1f32;
+        }
+        if self.rect.x > screen_width() - self.rect.w{
+            self.rect.x = -1f32;
+        }
+        if self.rect.y < 0f32{
+            self.rect.y = 1f32;
+        }
+    }
+}
+
 #[macroquad::main("breakout")]
 async fn main() {
     let mut player = Player::new();
+    let mut blocks = Vec::new();
+    let mut balls = Vec::new();
+
+    let (width, height) = (6,6);
+    let padding = 5f32;
+    let total_block_size = BLOCK_SIZE + vec2(padding, padding);
+    let board_start_pos = vec2((screen_width() - (total_block_size.x * width as f32)) / 2f32, 50f32);
+
+    for i in 0..width * height{
+        let block_x = ( i % width) as f32 * total_block_size.x;
+        let block_y = (i / width) as f32 * total_block_size.y;
+        blocks.push(Block::new(board_start_pos +vec2(block_x, block_y)));
+    }
+
+    balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
 
     loop{
+
+        if is_key_pressed(KeyCode::Space){
+            balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
+        }
         player.update(get_frame_time());
+
+        for ball in balls.iter_mut(){
+            ball.update(get_frame_time());
+        }
+
+
         clear_background(WHITE);
         player.draw();
+        for block in blocks.iter(){
+            block.draw();
+        }
+        for ball in balls.iter(){
+            ball.draw();
+        }
         next_frame().await
     }
 }
