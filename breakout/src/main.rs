b7896302d629f7be8428f59b5b1c414a52d5e434
sriram N V <sriramnvilvanathan@gmail.com>
2025-03-18 20:45:23 +0530

    added collision, ball movements, game states, player lives

--- breakout/src/main.rs
+++ breakout/src/main.rs
@@ -1,3 +1,4 @@
+
 use macroquad::prelude::*;
 
 const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
@@ -6,6 +7,13 @@ const BLOCK_SIZE: Vec2 = Vec2::from_array([110f32, 40f32]);
 const BALL_SIZE: f32 = 50f32;
 const BALL_SPEED: f32 = 400f32;
 
+
+pub enum GameState{
+    Menu,
+    Game,
+    LevelCompleted,
+    Dead,
+}
 struct Player{
     rect: Rect,
 }
@@ -23,14 +31,7 @@ impl Player{
     }
 
     pub fn update(&mut self, dt: f32){
-        // let mut x_move = 0f32;
-        // if is_key_down(KeyCode::Left){
-        //     x_move -= 1f32;
-        // }
-        // if is_key_down(KeyCode::Right){
-        //     x_move += 1f32;
-        // }
-
+        
         let x_move = match (is_key_down(KeyCode::Left), is_key_down(KeyCode::Right)){
             (true, false) => -1f32,
             (false, true) => 1f32,
@@ -54,17 +55,23 @@ impl Player{
 
 struct Block{
     rect: Rect,
+    lives: i32,
 }
 
 impl Block{
     pub fn new(pos: Vec2) -> Self{
         Self{
             rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y),
+            lives: 2,
         }
     }
 
     pub fn draw(&self){
-        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, GRAY);
+        let color = match self.lives{
+            2 => RED,
+            _ => ORANGE,
+        };
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, color);
     }
 }
 
@@ -92,16 +99,49 @@ impl Ball{
             self.vel.x = 1f32;
         }
         if self.rect.x > screen_width() - self.rect.w{
-            self.rect.x = -1f32;
+            self.vel.x = -1f32;
         }
         if self.rect.y < 0f32{
-            self.rect.y = 1f32;
+            self.vel.y = 1f32;
         }
     }
 }
 
+
+fn resolve_collision(a: &mut Rect, vel: & mut Vec2, b: &Rect) -> bool{
+    let intersection = match a.intersect(*b){
+        Some(intersection) => intersection,
+        None => return false,
+    };
+
+    if let Some(intersection) = a.intersect(*b){
+        let a_center = a.center();
+        let b_center = b.center();
+        let to = b_center - a_center;
+        let to_signum = to.signum();
+        match intersection.w > intersection.h{
+            true => {
+                a.y -= to_signum.y * intersection.h;
+                vel.y = -to_signum.y * vel.y.abs();
+            }
+            false => {
+                a.x -= to_signum.x * intersection.w;
+                vel.x = -to_signum.x * vel.x.abs();
+            }
+        }
+        
+    }
+    true
+}
+
+
 #[macroquad::main("breakout")]
 async fn main() {
+    let fontt= load_ttf_font("res/Heebo-VariableFont_wght.ttf").await.unwrap();
+    let mut game_state = GameState::Menu;
+    let mut score = 0;
+    let mut player_lives = 3;
+
     let mut player = Player::new();
     let mut blocks = Vec::new();
     let mut balls = Vec::new();
@@ -117,12 +157,12 @@ async fn main() {
         blocks.push(Block::new(board_start_pos +vec2(block_x, block_y)));
     }
 
-    balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
+    balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32 + 20f32)));
 
     loop{
 
         if is_key_pressed(KeyCode::Space){
-            balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
+            balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32 + 20f32)));
         }
         player.update(get_frame_time());
 
@@ -130,6 +170,27 @@ async fn main() {
             ball.update(get_frame_time());
         }
 
+        for ball in balls.iter_mut(){
+            resolve_collision(&mut ball.rect, &mut ball.vel, &player.rect);
+            for block in blocks.iter_mut(){
+                if resolve_collision(&mut ball.rect, &mut ball.vel, &block.rect){
+                block.lives -= 1;
+                if block.lives <= 0{
+                    score += 10;    
+                }
+                }
+            }
+        }
+
+        let ball_len = balls.len();
+        let last_ball = ball_len == 1;
+        balls.retain(|ball| ball.rect.y < screen_height() - 100f32);
+        let rem_ball = ball_len - balls.len();
+        if rem_ball > 0 && last_ball{
+            player_lives -= 1;
+        }
+
+        blocks.retain(|block| block.lives > 0);
 
         clear_background(WHITE);
         player.draw();
@@ -139,6 +200,41 @@ async fn main() {
         for ball in balls.iter(){
             ball.draw();
         }
+
+        match game_state{
+            GameState::Menu => {
+                let text = "Press Space to start";
+                let dims = measure_text(text, Some(&fontt), 50u16, 1.0f32);
+                draw_text_ex(
+                    text,
+                    screen_width() * 0.5f32 - dims.width * 0.5f32,
+                    screen_height() * 0.5f32 - dims.height * 0.5f32,
+                    TextParams { font: Some(&fontt), font_size: 50u16, color: BLACK, ..Default::default() },
+                );
+            },
+            GameState::Game => {
+                let score_text = format!("score: {}", score);
+                let score_text_dim = measure_text(&score_text, Some(&fontt), 30u16, 1.0);
+
+                draw_text_ex(
+                    &format!("Score: {}", score),
+                    screen_width() * 0.5f32 - score_text_dim.width * 0.5f32,
+                    40.0,
+                    TextParams { font: Some(&fontt), font_size: 30u16, color: BLACK, ..Default::default() },
+                );
+
+                draw_text_ex(
+                    &format!("lives: {}", player_lives),
+                    30.0,
+                    40.0,
+                    TextParams { font: Some(&fontt), font_size: 30u16, color: BLACK, ..Default::default() },
+                );
+                    },
+                    GameState::LevelCompleted => {},
+                    GameState::Dead => {}, 
+                }
+
+        
         next_frame().await
     }
 }
