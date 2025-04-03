888a43993c5bbbd5b7e7304ae463fa6eac41374c
sriram N V <sriramnvilvanathan@gmail.com>
2025-04-03 16:29:46 +0530

    Ownership concepts

--- learning/src/ownership.rs
+++ learning/src/ownership.rs
@@ -9,12 +9,21 @@
 fn main(){
     let s1 = String::from("hello");
     let s2 = s1;
+    let s3 = String::from("hello world");
     println!("s2: {}", s2);  // Rule 2:  There can only be one owner at a time. it will throw an error if s1 is printed as the ownership of s1 is transferred to s2
     let len = calculate_length(&s2);
     println!("The length of '{}' is {}.", s2, len); // Rule 1:  Each value in rust has a variable that is called its owner.
+
+    let len1 = calculate_length(&s3);
+    println!("The length of '{}' is {}.", s3, len1);
+    printloss(&s3);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn printloss(s: &String) {
+    println!("s: {}", s);
 }
 
